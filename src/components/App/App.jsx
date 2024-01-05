ae8c2f67e22477c1877a91d53aac10e4929ebd11
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-01-05 13:08:23 +0100

    Add components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,28 @@
+import user from '../../data/user.json';
+import data from '../../data/data.json'
+import friends from '../../data/friends.json'
+import transaction from '../../data/transactions.json'
+import Profile from '../Profile/Profile';
+import Statictics from '../Statistics/Statistics';
+import FriendList from '../FriendList/FriendList';
+import { Container } from './App.styled';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
+
+const App = () => {
+  return (
+    <Container>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+        avatar={user.avatar}
+      />
+      <Statictics title="Upload Stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transaction} />
+    </Container>
+    
+)};
+
+export default App;
